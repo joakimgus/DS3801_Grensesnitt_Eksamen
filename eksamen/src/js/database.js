6a95d650712a4b527ee74fd98b1d2d22d2fb637a
Tina Eilertsen <tinaeilertsen@outlook.com>
2020-10-19 19:01:05 +0200

    added productImage to products

--- eksamen/src/js/database.js
+++ eksamen/src/js/database.js
@@ -1,11 +1,11 @@
-var users = [
+let users = [
     {
         id: 1,
         firstName: "Tina",
         lastName: "Eilertsen",
         userName: "tinaeile",
         password: "qwerty1234",
-        userimg: "../img/user/female.png",
+        userimg: "src/img/user/female.png",
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ var users = [
         lastName: "Gustavsen",
         userName: "joakimgus",
         password: "qwerty1234",
-        userimg: "../img/user/male.png",
+        userimg: "src/img/user/male.png",
     },
     {
         id: 3,
@@ -21,7 +21,7 @@ var users = [
         lastName: "Buzza",
         userName: "nightDuck",
         password: "qwerty1234",
-        userimg: "../img/user/male.png",
+        userimg: "src/img/user/male.png",
     },
     {
         id: 4,
@@ -29,7 +29,7 @@ var users = [
         lastName: "Gjøstøl",
         userName: "fryLama",
         password: "qwerty1234",
-        userimg: "../img/user/male.png",
+        userimg: "src/img/user/male.png",
     },
 ];
 
@@ -41,7 +41,7 @@ var menu = [
                     {
                         productId: 1,
                         productName: "Black Coffee",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/black-coffee.jpeg",
                         price: [28, 32, 36],
                         allergies: "none",
                         description: "to be continued..",
@@ -50,7 +50,7 @@ var menu = [
                     {
                         productId: 2,
                         productName: "Americano",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/americano.jpg",
                         price: [32, 36, 40],
                         allergies: "none",
                         description: "to be continued..",
@@ -59,7 +59,7 @@ var menu = [
                     {
                         productId: 3,
                         productName: "Espresso",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/espresso.jpg",
                         price: [34, 38, 42],
                         allergies: "none",
                         description: "to be continued..",
@@ -68,7 +68,7 @@ var menu = [
                     {
                         productId: 4,
                         productName: "Cortado",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/cortado.jpg",
                         price: [36, 40, 44],
                         allergies: "none",
                         description: "to be continued..",
@@ -77,7 +77,7 @@ var menu = [
                     {
                         productId: 5,
                         productName: "Café Latte",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/Café-Latte.jpeg",
                         price: [38, 44, 48],
                         allergies: "Milk",
                         description: "to be continued..",
@@ -86,7 +86,7 @@ var menu = [
                     {
                         productId: 6,
                         productName: "Cappuccino",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/cappuccino.jpeg",
                         price: [38, 42, 46],
                         allergies: "Milk",
                         description: "to be continued..",
@@ -95,7 +95,7 @@ var menu = [
                     {
                         productId: 7,
                         productName: "Café Mocha",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/Café-Mocha.jpg",
                         price: [38, 42, 46],
                         allergies: "Milk, Cocoa powder",
                         description: "to be continued..",
@@ -104,7 +104,7 @@ var menu = [
                     {
                         productId: 8,
                         productName: "Caramel Macchiato",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/Caramel-Macchiato.jpg",
                         price: [45, 48, 51],
                         allergies: "Milk",
                         description: "to be continued..",
@@ -115,7 +115,7 @@ var menu = [
                     {
                         productId: 9,
                         productName: "Iced Coffee",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/Iced-Coffee.jpg",
                         price: [36, 42, 50],
                         allergies: "none",
                         description: "to be continued..",
@@ -124,7 +124,7 @@ var menu = [
                     {
                         productId: 10,
                         productName: "Iced Latte",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/Iced-Latte.jpg",
                         price: [38, 44, 48],
                         allergies: "Milk",
                         description: "to be continued..",
@@ -133,7 +133,7 @@ var menu = [
                     {
                         productId: 11,
                         productName: "Iced Cappuccino",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/Iced-cappuccino.jpg",
                         price: [38, 42, 46],
                         allergies: "Milk",
                         description: "to be continued..",
@@ -142,7 +142,7 @@ var menu = [
                     {
                         productId: 12,
                         productName: "Iced Mocha",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/Iced-mocha.jpg",
                         price: [38, 42, 46],
                         allergies: "Milk, Cocoa powder",
                         description: "to be continued..",
@@ -151,7 +151,7 @@ var menu = [
                     {
                         productId: 13,
                         productName: "Iced Caramel Macchiato",
-                        // productImage: ,
+                        productImage: "src/img/app_POS/Iced-caramel-macchiato.jpg",
                         price: [45, 48, 51],
                         allergies: "Milk",
                         description: "to be continued..",
@@ -164,7 +164,7 @@ var menu = [
             {
                 productId: 14,
                 productName: "Lemon Iced Tea",
-                //productImage: ,
+                productImage: "src/img/app_POS/Lemon-Iced-Tea.jpg",
                 price: [38, 47, 55],
                 allergies: "Lemon",
                 description: "to be continued",
@@ -173,7 +173,7 @@ var menu = [
             {
                 productId: 15,
                 productName: "Peach Iced Tea",
-                //productImage: ,
+                productImage: "src/img/app_POS/Peach-Iced-tea.jpg",
                 price: [38, 47, 55],
                 allergies: "Peach",
                 description: "to be continued",
@@ -182,7 +182,7 @@ var menu = [
             {
                 productId: 16,
                 productName: "Raspberry Iced Tea",
-                //productImage: ,
+                productImage: "src/img/app_POS/Raspberry-Iced-Tea.jpg",
                 price: [38, 47, 55],
                 allergies: "Raspberry",
                 description: "to be continued",
@@ -191,7 +191,7 @@ var menu = [
             {
                 productId: 17,
                 productName: "Pineapple Iced Tea",
-                //productImage: ,
+                productImage: "src/img/app_POS/Pineapple-Iced-Tea.jpg",
                 price: [38, 47, 55],
                 allergies: "Pineapple",
                 description: "to be continued",
@@ -202,7 +202,7 @@ var menu = [
             {
                 productId: 18,
                 productName: "Scones",
-                //productImage: ,
+                productImage: "src/img/app_POS/scones.jpg",
                 price: 37,
                 allergies: "Wheat, Milk, Eggs, Butter, Cocoa powder",
                 description: "to be continued",
@@ -211,7 +211,7 @@ var menu = [
             {
                 productId: 19,
                 productName: "Croissant",
-                //productImage: ,
+                productImage: "src/img/app_POS/croissant.jpg",
                 price: 38,
                 allergies: "Wheat, Milk, Eggs, Butter",
                 description: "to be continued",
@@ -220,7 +220,7 @@ var menu = [
             {
                 productId: 20,
                 productName: "Brioche",
-                //productImage: ,
+                productImage: "src/img/app_POS/brioche.jpg",
                 price: 44,
                 allergies: "Wheat, Milk, Eggs, Butter",
                 description: "to be continued",
@@ -229,7 +229,7 @@ var menu = [
             {
                 productId: 21,
                 productName: "Cinnamon Bun",
-                //productImage: ,
+                productImage: "src/img/app_POS/Cinnamon-bun.jpg",
                 price: 46,
                 allergies: "Wheat, Milk, Eggs, Butter, Cinnamon",
                 description: "to be continued",
@@ -238,7 +238,7 @@ var menu = [
             {
                 productId: 22,
                 productName: "Brownie",
-                //productImage: ,
+                productImage: "src/img/app_POS/brownie.jpg",
                 price: 44,
                 allergies: "Wheat, Milk, Eggs, Butter, Cocoa powder",
                 description: "to be continued",
@@ -247,7 +247,7 @@ var menu = [
             {
                 productId: 23,
                 productName: "Oreo Slice",
-                //productImage: ,
+                productImage: "src/img/app_POS/oreo.jpg",
                 price: 54,
                 allergies: "Wheat, Milk, Eggs, Butter, but can also contain: Soy, Crustacean Shellfish, Peanuts, Tree nuts, Lupin, Mollusks, Sesame seeds",
                 description: "to be continued",
@@ -256,7 +256,7 @@ var menu = [
             {
                 productId: 24,
                 productName: "Chia pudding",
-                //productImage: ,
+                productImage: "src/img/app_POS/Chia-pudding.jpg",
                 price: 42,
                 allergies: "Chia seeds, Coconut, Cocoa powder",
                 description: "to be continued",
