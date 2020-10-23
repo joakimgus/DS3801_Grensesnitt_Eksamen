b661c5875de8ef0cea0b8da53ff028bc35ba24d6
Jan Robert <janern007@hotmail.com>
2020-10-23 13:33:24 +0200

    added alt property and text to fotter-icons.jsx and in the Footer.jsx element

--- eksamen/src/img/footer-icons/footer-icons.jsx
+++ eksamen/src/img/footer-icons/footer-icons.jsx
@@ -3,26 +3,31 @@ let icons = [
         key: 1,
         name: "home",
         image: require("./home2.png"),
+        alt: "navigate home"
     },
     {
         key: 2,
         name: "user",
         image: require("./user2.png"),
+        alt: "go to user profile"
     },
     {
         key: 3,
         name: "favorite",
         image: require("./favorite2.png"),
+        alt: "show your favorite items"
     },
     {
         key: 4,
         name: "cart",
         image: require("./shopping-cart2.png"),
+        alt: "navigate to your shopping cart"
     },
     {
         key: 5,
         name: "options",
-        image: require("./options2.png")
+        image: require("./options2.png"),
+        alt: "display your user settings"
     }
 ];
 
--- eksamen/src/js/components/Footer.jsx
+++ eksamen/src/js/components/Footer.jsx
@@ -18,11 +18,11 @@ const Footer = () =>{
     console.log(icons);
     return(
         <footer>
-            <div className="home"><img src={icons[0].image}></img></div>
-            <div className="user"><img src={icons[1].image}></img></div>
-            <div className="favorites"><img src={icons[2].image}></img></div>
-            <div className="shopping-cart"><img src={icons[3].image}></img></div>
-            <div className="options"><img src={icons[4].image}></img></div>
+            <div className="home"><img src={icons[0].image} alt={icons[0].alt}></img></div>
+            <div className="user"><img src={icons[1].image} alt={icons[1].alt}></img></div>
+            <div className="favorites"><img src={icons[2].image} alt={icons[2].alt}></img></div>
+            <div className="shopping-cart"><img src={icons[3].image} alt={icons[3].alt}></img></div>
+            <div className="options"><img src={icons[4].image} alt={icons[4].alt}></img></div>
         </footer>
     );
 }
