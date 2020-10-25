26ba1b31a1fbc1bda1908002403945345e876254
Tina Eilertsen <tinaeilertsen@outlook.com>
2020-10-25 18:17:32 +0100

    fixed mainMenu

--- eksamen/src/js/App.jsx
+++ eksamen/src/js/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
   return(
     <>
       <MainMenu />
+      <Footer />
     </>
   )
 }
--- eksamen/src/js/components/MainMenu.jsx
+++ eksamen/src/js/components/MainMenu.jsx
@@ -8,19 +8,20 @@ const MainMenu = () => {
   const foodImg = require("../../img/app_POS/food.jpeg");
 
   return(
-    <div id="menuCardContainer">
-      <div id="coffeeContainer">
-        <img className="mainMenuImg" src={coffeeImg}/>
-        <h1 classname="mainMenuText">COFFEE</h1>
+    <div className="homepage" id="menuCardContainer">
+
+      <div className="homepage" id="coffeeContainer">
+        <p className="mainMenuText">COFFEE</p>
       </div>
-      <div id="drinksContainer">
-        <img className="mainMenuImg" src={drinksImg}/>
-        <h1 classname="mainMenuText">DRINKS</h1>
+
+      <div className="homepage" id="drinksContainer">
+        <p className="mainMenuText">DRINKS</p>
       </div>
-      <div id="foodContainer">
-        <img className="mainMenuImg" src={foodImg}/>
-        <h1 classname="mainMenuText">BAKED GOODS</h1>
+
+      <div className="homepage" id="foodContainer">
+        <p className="mainMenuText">BAKED GOODS</p>
       </div>
+
     </div>
   )
 }
