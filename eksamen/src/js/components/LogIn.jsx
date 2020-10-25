dd370df41475194226d1928a1c34e1f4c31694cd
Tina Eilertsen <tinaeilertsen@outlook.com>
2020-10-25 15:21:25 +0100

    fixed login page

--- eksamen/src/js/components/LogIn.jsx
+++ eksamen/src/js/components/LogIn.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import '../../css/Login.css';
 
 const Login = () => {
   const imgPath = require("../../img/app_POS/logo.png");
   return(
-    <div>
-      <img src={imgPath} />
+    <div className="logoBody">
+      <img id="logo" src={imgPath} />
       <h1>iCafé</h1>
-      <h3>USERNAME</h3>
-      <input></input>
-      <a>Continue as guest</a>
+      <label id="unameLabel">USERNAME</label> <br />
+      <input id="uname" placeholder="Username"></input> <br />
+      <label>PASSWORD</label> <br />
+      <input id="pw" placeholder="Password"></input> <br />
+      <button id="login-btn">Login</button>
+      <button id="signup-btn">Sign Up</button> <br />
+      <a href="">Continue as guest</a>
     </div>
   )
 }
