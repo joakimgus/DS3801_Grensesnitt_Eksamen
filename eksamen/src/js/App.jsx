58864ea2a077c2f47026dafda0aee6805154bd7b
joakimgus <joakim.gus95@gmail.com>
2020-10-25 13:08:02 +0100

    made login.jsx

--- eksamen/src/js/App.jsx
+++ eksamen/src/js/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/App.css';
 import MenuFilter from './components/MenuFilter.jsx';
+import LogIn from './components/LogIn.jsx';
 
 const App = () => {
   return(
---
+++ eksamen/src/js/components/LogIn.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LogIn = () => {
+  return(
+    <div>
+      <h1>iCafé</h1>
+      <h3>USERNAME</h3>
+      <input></input>
+      <a>Continue as guest</a>
+    </div>
+  )
+}
+
+export default LogIn
