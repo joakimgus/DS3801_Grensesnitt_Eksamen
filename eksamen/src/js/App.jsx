1c06698f0d0a3e60fb98370ebe091abe2a303374
joakimgus <joakim.gus95@gmail.com>
2020-10-25 12:52:10 +0100

    moved menu filter to own component

--- eksamen/src/js/App.jsx
+++ eksamen/src/js/App.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
 import '../css/App.css';
-import { menuItems } from './database';
+import MenuFilter from './components/MenuFilter.jsx';
 
 const App = () => {
-  const filterHotCoffee = menuItems.filter(menuItem => menuItem.category === "hotCoffee");
-
-    return(
-        <div>
-          {filterHotCoffee.map(menuItem => {
-            return(
-              <div className="productCard" key={menuItem.productId}>
-                <img src={menuItem.productImage} alt={menuItem.productName} />
-                <h1 className="productTitle">{menuItem.productName}</h1>
-                <p className="productPrice">From kr. {menuItem.price[0]},-</p>
-              </div>
-            )
-          })} 
-        </div>
-      )
+  return(
+    <>
+      <MenuFilter />
+    </>
+  )
 }
 
 export default App
-
-
-// ha alle items i et array, .filter == category for hva som vises
-// filter based på hva som blir klikket på 
-// https://stackoverflow.com/questions/59819613/map-filter-in-react-objects-are-not-valid-as-a-react-child-if-you-meant-to-ren
