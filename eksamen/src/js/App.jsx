deca72ac04ab23e1c8d87b59589c88a497be0643
Jan Robert <janern007@hotmail.com>
2020-10-26 16:05:39 +0100

    user page with retard profile picture and emoji-placeholders for navigations and call-to-action. Button is dead - only visuals

--- eksamen/src/js/App.jsx
+++ eksamen/src/js/App.jsx
@@ -4,11 +4,13 @@ import ProductRender from './components/ProductRender.jsx';
 import Login from './components/Login.jsx';
 import Footer from './components/Footer.jsx';
 import MainMenu from './components/MainMenu.jsx';
+import UserProfile from './components/UserProfile.jsx';
 
 const App = () => {
   return(
     <>
       <ProductRender />
+      <UserProfile />
       <Footer />
     </>
   )
