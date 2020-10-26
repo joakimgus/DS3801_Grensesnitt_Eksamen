626842bc23abc20c5a1e6d341422bf4aaf7647f7
joakimgus <joakim.gus95@gmail.com>
2020-10-26 15:37:47 +0100

    fixed header to change color based on what is pressed

--- eksamen/src/js/App.jsx
+++ eksamen/src/js/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../css/App.css';
 import Header from './components/Header.jsx';
-import ProductRender from './components/ProductRender.jsx';
 import Login from './components/Login.jsx';
-import Footer from './components/Footer.jsx';
 import MainMenu from './components/MainMenu.jsx';
+import ProductRender from './components/ProductRender.jsx';
+import Footer from './components/Footer.jsx';
 
 
 const App = () => {
