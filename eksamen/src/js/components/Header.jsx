4dd6a309779a97a221ad95f55e9b70d27dbb34b9
joakimgus <joakim.gus95@gmail.com>
2020-10-26 13:02:03 +0100

    more header

--- eksamen/src/js/components/Header.jsx
+++ eksamen/src/js/components/Header.jsx
@@ -4,7 +4,7 @@ import '../../css/Header.css';
 const Header = () => {
 
   return(
-    <div>
+    <div id="HeaderContainer">
       <button id="tabOne" className="headerBtn">HOT</button>
       <button id="tabTwo" className="headerBtn">ICED</button>
     </div>
