f4d11496cdd0fb16c2489f76add3a532701c3c63
joakimgus <joakim.gus95@gmail.com>
2020-10-26 13:00:23 +0100

    Started work onHeader

--- eksamen/src/js/App.jsx
+++ eksamen/src/js/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import '../css/App.css';
+import Header from './components/Header.jsx';
 import ProductRender from './components/ProductRender.jsx';
 import Login from './components/Login.jsx';
 import Footer from './components/Footer.jsx';
 import MainMenu from './components/MainMenu.jsx';
 
+
 const App = () => {
   return(
     <>
+      <Header />
       <ProductRender />
       <Footer />
     </>
---
+++ eksamen/src/js/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../css/Header.css';
+
+const Header = () => {
+
+  return(
+    <div>
+      <button id="tabOne" className="headerBtn">HOT</button>
+      <button id="tabTwo" className="headerBtn">ICED</button>
+    </div>
+  )
+
+}
+
+export default Header;
