cd3286ed381080bb860e8bc2c0117d25af33a806
Isar <isar.buzza@gmail.com>
2020-11-02 11:15:46 +0100

    added POS footer component

--- eksamen/src/js/components/POS_Footer.jsx
+++ eksamen/src/js/components/POS_Footer.jsx
@@ -5,7 +5,7 @@ import '../../css/POS_Footer.css';
 
 
 const POS_Footer = (props) =>{
-    //console.log(icons);
+    
 
 
     return(
@@ -17,14 +17,14 @@ const POS_Footer = (props) =>{
                <img src={posFooterIcons[0].image} alt={posFooterIcons[0].alt}></img>
                <p>Coffee</p>
             </div>
-            <div className="POS_Footer" id="favorites"><img src={posFooterIcons[1].image} alt={posFooterIcons[1].alt}></img>
+            <div className="POS_Footer" id="drinks"><img src={posFooterIcons[1].image} alt={posFooterIcons[1].alt}></img>
             <p>Drinks</p>
             </div>
             <div className="POS_Footer" id="food"><img src={posFooterIcons[3].image} alt={posFooterIcons[3].alt}></img>
             <p>Food</p>
             </div>
-            <div className="POS_Footer" id="options"><img className="POS_Button" src={posFooterIcons[2].image} alt={posFooterIcons[2].alt}></img></div>
-            <div className="POS_Footer" id="calculator"><img className="POS_Button" src={posFooterIcons[5].image} alt={posFooterIcons[5].alt}></img></div>
+            <div className="POS_Footer" id="orders"><img className="POS_Button" src={posFooterIcons[2].image} alt={posFooterIcons[2].alt}></img></div>
+            <div className="POS_Footer" id="receipts"><img className="POS_Button" src={posFooterIcons[5].image} alt={posFooterIcons[5].alt}></img></div>
             <div className="POS_Footer" id="calculator"><img className="POS_Button" src={posFooterIcons[4].image} alt={posFooterIcons[4].alt}></img></div>
         </nav>
     );
