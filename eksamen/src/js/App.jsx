ce91b32c58f7dfd6199e798065c94e4cc43e48e6
Jan Robert <janern007@hotmail.com>
2020-11-04 18:32:29 +0100

    Added "mainMeu" render to teniary operator at app.jsx

--- eksamen/src/js/App.jsx
+++ eksamen/src/js/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
 
   return(
     <>
-      {(where) === 'renderProfile' ? <UserProfile /> : (where) === 'home' ? <ProductRender /> : <ProductRender />} 
+      {(where) === 'renderProfile' ? <UserProfile /> : (where) === 'renderHome' ? <MainMenu /> : <ProductRender />} 
       <Footer
         data={`${where}`}
         onClick={handleClick}
