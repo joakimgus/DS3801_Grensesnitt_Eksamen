5e42ffbe5764aaf564e90a4718920a8d7bb5b080
joakimgus <joakim.gus95@gmail.com>
2020-11-07 14:08:08 +0100

    fixed food products to correctly show price

--- eksamen/src/js/components/ProductCard.jsx
+++ eksamen/src/js/components/ProductCard.jsx
@@ -27,18 +27,32 @@ const List = ({list, setList, filteredList}) => {
   return(
     <>
       {filteredList.map((menuItem) => (
-        <div className="productCard" key={menuItem.productId}>
-          <img className="productImage" src={menuItem.productImage} alt={menuItem.productName} />
-          <h1 className="productTitle">{menuItem.productName}</h1>
-          <p className="productPrice">From {menuItem.price[0]},-</p>
-          <img id="arrow" alt="arrow" src={require("../../img/app_POS/arrow.png")} alt="arrow-selector" />
-          <img 
-            id="favorite"
-            alt="favorite"
-            src={menuItem.favourite ? require("../../img/app_POS/favoriteTrue.png") : require("../../img/app_POS/favoriteFalse.png")}
-            onClick={() => handleToggleComplete(menuItem.productId)}
-          />
-        </div>
+        menuItem.category === "food" ?
+          <div className="productCard" key={menuItem.productId}>
+            <img className="productImage" src={menuItem.productImage} alt={menuItem.productName} />
+            <h1 className="productTitle">{menuItem.productName}</h1>
+            <p className="productPrice">From {menuItem.price},-</p>
+            <img id="arrow" alt="arrow" src={require("../../img/app_POS/arrow.png")} alt="arrow-selector" />
+            <img 
+              id="favorite"
+              alt="favorite"
+              src={menuItem.favourite ? require("../../img/app_POS/favoriteTrue.png") : require("../../img/app_POS/favoriteFalse.png")}
+              onClick={() => handleToggleComplete(menuItem.productId)}
+            />
+          </div>
+        :
+          <div className="productCard" key={menuItem.productId}>
+            <img className="productImage" src={menuItem.productImage} alt={menuItem.productName} />
+            <h1 className="productTitle">{menuItem.productName}</h1>
+            <p className="productPrice">From {menuItem.price[0]},-</p>
+            <img id="arrow" alt="arrow" src={require("../../img/app_POS/arrow.png")} alt="arrow-selector" />
+            <img 
+              id="favorite"
+              alt="favorite"
+              src={menuItem.favourite ? require("../../img/app_POS/favoriteTrue.png") : require("../../img/app_POS/favoriteFalse.png")}
+              onClick={() => handleToggleComplete(menuItem.productId)}
+            />
+          </div>
       ))}
     </>
     )
