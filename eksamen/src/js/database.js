c27d4e8806179a0ee77b05590451fd5d723fc83f
Jan Robert <janern007@hotmail.com>
2020-11-07 17:17:38 +0100

    Single item view is now available

--- eksamen/src/js/database.js
+++ eksamen/src/js/database.js
@@ -433,4 +433,17 @@ const posCartIcons = [
     }
 ]
 
-export {users, menuItems, footerIcons, posFooterIcons, cartIcons, orderHistory, posCartIcons};
+let singleItem = [
+    {
+        productId: 1,
+        productName: "Black Coffee",
+        productImage: require("../img/app_POS/black-coffee.jpeg"),
+        price: [28, 32, 36],
+        allergies: "none",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also",
+        favourite: false,
+        category: "hotCoffee"
+    }
+];
+
+export {users, menuItems, footerIcons, posFooterIcons, cartIcons, orderHistory, posCartIcons, singleItem};
