b8e7f44e4d4f6d528c9a0c1695e8ec15f44bfef5
joakimgus <joakim.gus95@gmail.com>
2020-11-08 14:53:43 +0100

    fixed pricing on "foods"

--- eksamen/src/js/components/mobile/ProductCard.jsx
+++ eksamen/src/js/components/mobile/ProductCard.jsx
@@ -66,38 +66,61 @@ function addItemToCart(){
   return(
     <>
       {all != "single" ? filteredList.map((menuItem) => (
-        <div className="productCard" key={menuItem.productId}>
-          <img className="productImage" src={menuItem.productImage} alt={menuItem.productName} />
-          <h1 className="productTitle">{menuItem.productName}</h1>
-          <p className="productPrice">From {menuItem.price[0]},-</p>
-          <img 
-            id="arrow" 
-            alt="arrow" 
-            src={require("../../../img/mobile-icons/arrow.png")} 
-            data-product={menuItem.productName} 
-            data-list={menuItem.category}
-            onClick={handleProductView} 
-          />
-          <img 
-            id="favorite"
-            alt="favorite"
-            src={menuItem.favourite ? require("../../../img/mobile-icons/favourite-true.png") : require('../../../img/mobile-icons/favourite-false.png')}
-            onClick={() => handleToggleComplete(menuItem.productId)}
-          />
-        </div>
-      )) : <div>
-            <div className="single-view-container" key={itemHolder[length].productId}>
+        (menuItem.category != "food" ? 
+          <div className="productCard" key={menuItem.productId}>
+            <img className="productImage" src={menuItem.productImage} alt={menuItem.productName} />
+            <h1 className="productTitle">{menuItem.productName}</h1>
+            <p className="productPrice">From {menuItem.price[0]},-</p>
+            <img 
+              id="arrow" 
+              alt="arrow" 
+              src={require("../../../img/mobile-icons/arrow.png")} 
+              data-product={menuItem.productName} 
+              data-list={menuItem.category}
+              onClick={handleProductView} 
+            />
+            <img 
+              id="favorite"
+              alt="favorite"
+              src={menuItem.favourite ? require("../../../img/mobile-icons/favourite-true.png") : require('../../../img/mobile-icons/favourite-false.png')}
+              onClick={() => handleToggleComplete(menuItem.productId)}
+            />
+          </div>
+        : 
+          <div className="productCard" key={menuItem.productId}>
+            <img className="productImage" src={menuItem.productImage} alt={menuItem.productName} />
+            <h1 className="productTitle">{menuItem.productName}</h1>
+            <p className="productPrice">From {menuItem.price},-</p>
+            <img 
+              id="arrow" 
+              alt="arrow" 
+              src={require("../../../img/mobile-icons/arrow.png")} 
+              data-product={menuItem.productName} 
+              data-list={menuItem.category}
+              onClick={handleProductView} 
+            />
+            <img 
+              id="favorite"
+              alt="favorite"
+              src={menuItem.favourite ? require("../../../img/mobile-icons/favourite-true.png") : require('../../../img/mobile-icons/favourite-false.png')}
+              onClick={() => handleToggleComplete(menuItem.productId)}
+            />
+          </div>
+        ))) 
+        : 
+        <div>
+          <div className="single-view-container" key={itemHolder[length].productId}>
             <button className="return-button" onClick={handleSetBackToAll} alt="return to favorites button">⬅</button>
-                <img className="imgProduct" src={itemHolder[length].productImage} alt={itemHolder[length].productName} />
-                <h3 className="productName">{itemHolder[length].productName}</h3>
-                <p className="productDescription">{itemHolder[length].description}</p>
+            <img className="imgProduct" src={itemHolder[length].productImage} alt={itemHolder[length].productName} />
+            <h3 className="productName">{itemHolder[length].productName}</h3>
+            <p className="productDescription">{itemHolder[length].description}</p>
 
 
-                {/* Buttons for Quantity/Shots/Size/Milk */}
-                <div className="tweakContainer">
-                </div>
-                <button className="add-to-cart" onClick={addItemToCart}>add to cart</button>
+            {/* Buttons for Quantity/Shots/Size/Milk */}
+            <div className="tweakContainer">
             </div>
+            <button className="add-to-cart" onClick={addItemToCart}>add to cart</button>
+          </div>
         </div>
       }
     </>
