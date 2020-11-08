fdfdb20ddde0bb923149fdfdcff97a4a2b30413d
Tina Eilertsen <tinaeilertsen@outlook.com>
2020-11-09 00:01:47 +0100

    wups, setup for devbranch, forgot i was in master

--- eksamen/src/js/Pos.jsx
+++ eksamen/src/js/Pos.jsx
@@ -13,6 +13,7 @@ import POS_Drinks from './components/POS/POS_Drinks';
 import POS_ProductCard from './components/POS/POS_ProductCard.jsx';
 import POS_ShoppingCart from './components/POS/POS_ShoppingCart.jsx';
 import POS_WorkerSelect from './components/POS/POS_WorkerSelect.jsx';
+import POS_Calculator from './components/POS/POS_Calculator';
 
 
 
@@ -53,7 +54,7 @@ const Pos = (props) => {
         <POS_Header />
         <POS_WorkerSelect />
         <POS_ShoppingCart />
-        <POS_Coffee />
+        <POS_Calculator />
         <POS_Footer />
       </div>
     </>
