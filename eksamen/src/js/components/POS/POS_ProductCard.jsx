1d5a5a8e36c90f9627b3a372afa3063a9d4a11bb
Jan Robert <janern007@hotmail.com>
2020-11-09 00:23:41 +0100

    items are now begin added to POS_cart on "add to cart"

--- eksamen/src/js/components/POS/POS_ProductCard.jsx
+++ eksamen/src/js/components/POS/POS_ProductCard.jsx
@@ -2,10 +2,21 @@ import { menuItems } from '../../database';
 import React from 'react';
 import '../../../css/POS/POS_ProductCard.css';
 
-const POS_ProductCard = ({ posList, setPosList, filteredPosList }) => {
+const POS_ProductCard = ({ posList, setPosList, filteredPosList }, props) => {
 
 
 
+
+function addItemToCart(e){
+  let target = e.target.getAttribute('data-item');
+  let temp = JSON.parse(localStorage.getItem('POS_cart')) || [];
+
+  filteredPosList.map(
+    (singleItem) => singleItem.productName === target ? temp.push(singleItem) : null
+  );
+
+  localStorage.setItem('POS_cart', JSON.stringify(temp));
+}
 
   return (
 
@@ -19,12 +30,14 @@ const POS_ProductCard = ({ posList, setPosList, filteredPosList }) => {
                 <img className="posProductImage" src={menuItem.productImage} />
                 <h1 className="posProductTitle">{menuItem.productName}</h1>
                 <p className="posProductPrice">From {menuItem.price[0]}.-</p>
+                <button onClick={addItemToCart} data-item={menuItem.productName}>Add to cart</button>
               </div>
               :
               <div className="posProductCard">
                 <img className="posProductImage" src={menuItem.productImage} />
                 <h1 className="posProductTitle">{menuItem.productName}</h1>
                 <p className="posProductPrice">{menuItem.price}.-</p>
+                <button onClick={addItemToCart} data-item={menuItem.productName}>Add to cart</button>
               </div>
           ))}
         </div>
