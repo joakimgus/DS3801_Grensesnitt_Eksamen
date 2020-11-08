13c479f6eb5f8f0acf8c2e65976cb23f0319b03d
Tina Eilertsen <tinaeilertsen@outlook.com>
2020-11-08 16:43:04 +0100

    fixed grid for pos system, placed all components inside grid

--- eksamen/src/index.jsx
+++ eksamen/src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/App';
+import POS_App from './js/POS_App';
 
 
 ReactDOM.render(
   <div>
-    <App />
+    <POS_App />
   </div>,
   document.getElementById('root')
 );
