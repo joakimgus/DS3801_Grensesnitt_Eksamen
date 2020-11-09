9083670cae82543aa48afa834e85bc7d08589409
Tina Eilertsen <tinaeilertsen@outlook.com>
2020-11-09 01:27:35 +0100

    fix buttons ++

--- eksamen/src/js/components/POS/POS_ProductCard.jsx
+++ eksamen/src/js/components/POS/POS_ProductCard.jsx
@@ -30,14 +30,14 @@ const POS_ProductCard = ({ posList, setPosList, filteredPosList }, props) => {
                 <img className="posProductImage" src={menuItem.productImage} />
                 <h1 className="posProductTitle">{menuItem.productName}</h1>
                 <p className="posProductPrice">From {menuItem.price[0]}.-</p>
-                <button onClick={addItemToCart} data-item={menuItem.productName}>Add to cart</button>
+                <button onClick={addItemToCart} data-item={menuItem.productName}>add</button>
               </div>
               :
               <div className="posProductCard">
                 <img className="posProductImage" src={menuItem.productImage} />
                 <h1 className="posProductTitle">{menuItem.productName}</h1>
                 <p className="posProductPrice">{menuItem.price}.-</p>
-                <button onClick={addItemToCart} data-item={menuItem.productName}>Add to cart</button>
+                <button onClick={addItemToCart} data-item={menuItem.productName}>add</button>
               </div>
           ))}
         </div>
